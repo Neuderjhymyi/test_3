9d7caf4f884c1900b5eae0f413129f8436b1a073
Neuderjhymyi <gosha0412@gmail.com>
2023-06-09 19:21:09 +0400

    Modified_2 function in main.py file

--- main.py
+++ main.py
@@ -3,9 +3,10 @@
 hello = 'Hello, Synergy!'
 
 def print_hello(args):
+    input("press any key and enter: ")
     for i in range(10):
         print(hello)
-    return input("press any key to exit")
+    return input("press any key and enter to exit: ")
 
 
 print_hello(hello)
